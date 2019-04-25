9a4f153d6b8ce0c08eda52d025fc55e3e5781332
Zozan CAN <zzncan@gmail.com>
2019-04-25 21:02:10 +0300

    download to first data

--- app/src/main/java/com/zozancan/dovizcevirici/MainActivity.java
+++ app/src/main/java/com/zozancan/dovizcevirici/MainActivity.java
@@ -20,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void cevir (View view) {
 
+        VeriIndir veriIndir = new VeriIndir();
+
+        try {
+
+            String url = "http://data.fixer.io/api/latest?access_key=4c1ce97fd4eb7e86fa35bc7316bb687b&format=1";
+
+            veriIndir.execute(url);
+
+        } catch (Exception e) {
+
+        }
+
     }
 
     public class VeriIndir extends AsyncTask<String, Void,String> {
@@ -38,6 +50,16 @@ public class MainActivity extends AppCompatActivity {
                 InputStream inputStream = httpURLConnection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 
+                int veri = inputStreamReader.read();
+
+                while (veri > 0) {
+
+                    char karakter = (char) veri;
+                    sonuc += karakter;
+
+                    veri = inputStreamReader.read();
+                }
+
                 return sonuc;
 
             } catch (Exception e) {
@@ -49,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+
+            System.out.println("Alınan data : " +s);
         }
     }
 }
