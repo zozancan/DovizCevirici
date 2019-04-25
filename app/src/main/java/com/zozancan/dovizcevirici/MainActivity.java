6637fc254225ae07034b8e649c1ffaba2b6f1cf5
Zozan CAN <zzncan@gmail.com>
2019-04-25 23:21:34 +0300

    data processed

--- app/src/main/java/com/zozancan/dovizcevirici/MainActivity.java
+++ app/src/main/java/com/zozancan/dovizcevirici/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,10 +15,22 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView cadText;
+    TextView chfText;
+    TextView usdText;
+    TextView jpyText;
+    TextView tryText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cadText = findViewById(R.id.cadText);
+        chfText = findViewById(R.id.chfText);
+        usdText = findViewById(R.id.usdText);
+        jpyText = findViewById(R.id.jpyText);
+        tryText = findViewById(R.id.tryText);
     }
 
     public void cevir (View view) {
@@ -72,7 +87,36 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-            System.out.println("Alınan data : " +s);
+            // System.out.println("Alınan data : " +s);
+
+            try {
+
+                JSONObject jsonObject = new JSONObject(s);
+                String base = jsonObject.getString("base");
+                String rates = jsonObject.getString("rates");
+
+                JSONObject jsonObject1 = new JSONObject(rates);
+
+                String turkLirasi =jsonObject1.getString("TRY");
+                tryText.setText("TRY : " + turkLirasi);
+
+                String usd =jsonObject1.getString("USD");
+                usdText.setText("USD : " + usd);
+
+                String cad =jsonObject1.getString("CAD");
+                cadText.setText("CAD : " + cad);
+
+                String jpy =jsonObject1.getString("JPY");
+                jpyText.setText("JPY : " + jpy);
+
+                String chf =jsonObject1.getString("CHF");
+                chfText.setText("CHF : " + chf);
+
+
+
+            } catch (Exception e) {
+
+            }
         }
     }
 }
